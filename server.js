a32bf4d93f3807073213d88dc8b29b579d2605b5
pobba-dud <ajbd47@gmail.com>
2024-11-14 09:18:40 -0500

    organization and server stuff

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-// Serve static files
-app.use(express.static('public'));
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Set up a basic route for your homepage
+// Route for the homepage
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Make the app listen on the port provided by Heroku
