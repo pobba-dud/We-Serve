800a0fddca07005dea3d8995638bd14551ce0e3f
pobba-dud <ajbd47@gmail.com>
2024-11-18 12:06:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,6 @@ app.post('/send-feedback', (req, res) => {
         return res.status(500).send('Error sending email: ' + error.message); // Send detailed error message
     }
     console.log('Email sent: ' + info.response);
-    res.status(200);
     stop
 });
 });
